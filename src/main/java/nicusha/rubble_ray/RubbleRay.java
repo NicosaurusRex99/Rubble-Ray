6c5c7d240540f3b39e74d77041458987c575379a
Nicusha <nicosaurusrex99@gmail.com>
2023-06-08 15:27:30 +0930

    ported

--- src/main/java/nicusha/rubble_ray/RubbleRay.java
+++ src/main/java/nicusha/rubble_ray/RubbleRay.java
@@ -1,12 +1,13 @@
 package nicusha.rubble_ray;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.*;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -18,6 +19,7 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 @Mod(RubbleRay.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = RubbleRay.MODID)
 public class RubbleRay
 {
     public static final String MODID = "rubble_ray";
@@ -27,32 +29,26 @@ public class RubbleRay
 
     public static final RegistryObject<Item> RUBBLE_RAY = ITEMS.register("rubble_ray", () -> new ItemRubbleRay());
     public static final RegistryObject<SoundEvent> RAY = SOUNDS.register("ray", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "ray")));
+    public static final DeferredRegister<CreativeModeTab> TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
+    public static final RegistryObject<CreativeModeTab> CREATIVE_TAB = TAB.register("tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.rubble_ray")).icon(() -> new ItemStack(RUBBLE_RAY.get())).build());
 
     public RubbleRay()
     {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        bus.addListener(RubbleRay::registerTab);
         ITEMS.register(bus);
         SOUNDS.register(bus);
+        TAB.register(bus);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC, MODID + "-common.toml");
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-
-    public static final ResourceLocation TAB = new ResourceLocation(RubbleRay.MODID, "rubble_ray");
-
-    private static ItemStack makeIcon() {
-        return new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(RubbleRay.MODID, "rubble_ray")));
-    }
-
-
-    public static void registerTab(CreativeModeTabEvent.Register event){
-        event.registerCreativeModeTab(TAB, builder -> builder.title(Component.translatable("itemGroup.rubble_ray")).icon(RubbleRay::makeIcon).displayItems((flags, output) -> {
-            for(RegistryObject<Item> item : RubbleRay.ITEMS.getEntries()){
-                output.accept(item.get());
+    @SubscribeEvent
+    public static void creativeTab(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab() == CREATIVE_TAB.get()) {
+            for (var regObj : ITEMS.getEntries()) {
+                event.accept(regObj.get());
             }
-        }));
-
+        }
     }
 }
